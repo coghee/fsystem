2c18ab98ebb369b7b3b860fdeb7b32fb58939314
coghee <coghee@github.com>
2014-12-06 15:31:57 +0900

    Made getattr, readdir function

--- main.c
+++ main.c
@@ -10,36 +10,101 @@ typedef struct{
 	char *name;
 	char *data;
 	off_t size;
+	mode_t mode;
 
 } file;
 
+file *file_list[200];
+int file_count = 0;
+
+file *getfile(const char *name){
+	file *target_file = NULL;
+	int i;
+	if(name!=NULL){
+		for(i=0; i<file_count; i++){
+			file *iter_file = file_list[i];
+			if(strcmp(iter_file->name, name)==0){
+				target_file = iter_file;
+			}
+		}
+	}
+	return target_file;
+}
+
 static int f_getattr(const char *path, struct stat *stbuf){
+	printf("@getattr");
 	int result = 0;
+	memset(stbuf, 0, sizeof (stbuf));
 
-	//
-	stbuf->st_mode = S_IFREG;
-	stbuf->st_nlink = 1;
-	stbuf->st_size = 0;
-
+	file *_file = getfile(path);
+	
+	if (strcmp(path, "/") == 0) {
+		stbuf->st_mode = S_IFDIR | 0755;
+		stbuf->st_nlink = 2;
+	} else if (_file != NULL) {
+		stbuf->st_mode = S_IFREG | 0444;
+		stbuf->st_nlink = 1;
+		stbuf->st_size = _file->size;
+	} else
+		result = -ENOENT;
+	
 	return result;
 }
 
 static int f_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi){
+	printf("@readdir");
 	int result = 0;
+	int i;
+	file *_file = getfile(path);
+
+	if (strcmp(path, "/") != 0)
+		return -ENOENT;
+
+	filler(buf, ".", NULL, 0);
+	filler(buf, "..", NULL, 0);
+	if(_file != NULL){	
+		
+		for(i=0; i<file_count; i++){
+			filler(buf, file_list[i]->name, NULL, 0);
+		}	
+	}
+	
 
 	return result;
 }
 
 static int f_open(const char *path, struct fuse_file_info *fi){
+	printf("@open");
 	int result = 0;
 
 	return result;
 }
 
 static int f_read(const char *path, char *buf, size_t size, off_t offset){
-	size = (sizeof) char;
+	printf("@read");
+	size = sizeof (char);
 
 	return size;
+}
+
+static int f_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
+	printf("@write");
+	return size;
+}
+
+static int f_create(const char *path, mode_t mode, struct  fuse_file_info *fi){
+	printf("@create");
+	int result = 0;
+
+	return result;
+
+}
+
+static int f_unlink(const char *path){
+	printf("@unlink");
+	int result = 0;
+
+	return result;
 }
 
 
@@ -49,6 +114,10 @@ static struct fuse_operations f_oper = {
 	.readdir		= f_readdir,
 	.open  		= f_open,
 	.read 		= f_read,
+	.write 		= f_write,
+	.create 		= f_create,
+	.unlink 		= f_unlink
+
 };
 
 int main(int argc, char *argv[]){
